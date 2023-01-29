1f7ad0752cd4ed27012a0a2586df05790ad474fd
Deeparokka5 <deeparokka98@gmail.com>
2023-01-29 23:15:25 +0545

    adding feature in home page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,2 +1,16 @@
+const watchlistIcon = document.querySelector('.watchlist-icon');
 
+watchlistIcon.addEventListener('click', function() {
+    window.location.href = "mylist.html";
+    var addIcon = watchlist.querySelector(".add-icon");
+  var checkIcon = watchlist.querySelector(".check-icon");
+  addIcon.style.display = "none";
+  checkIcon.style.display = "block";
+  // Perform action here
+});
 
+const watchlist = document.querySelector("#watchlist");
+
+watchlist.addEventListener("click", function() {
+  this.classList.toggle("added");
+});
