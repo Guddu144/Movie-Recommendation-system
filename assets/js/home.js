65f5c6bb70309ed8d9114513e303bf1e93b4d01c
Aditya <khadkaaditya983@gmail.com>
2023-01-30 21:06:35 +0545

    bookmark toggle fixed

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,13 +1,18 @@
-const watchlistIcon = document.querySelector('.watchlist-icon');
+const watchlistIcon = document.querySelector(".watchlist-icon");
 
-watchlistIcon.addEventListener('click', function() {
-    // window.location.href = "mylist.html";
-  
+watchlistIcon.addEventListener("click", function () {
+  // window.location.href = "mylist.html";
 });
 const bookmarkIcon = document.querySelector("#bookmark-icon");
 const popup = document.querySelector("#popup");
 
-bookmarkIcon.addEventListener("click", function() {
-  bookmarkIcon.style.color = "blue";
+document.getElementById("bookmark-icon").addEventListener("click", function () {
+  if (this.classList.contains("bookmark-icon-1")) {
+    this.classList.remove("bookmark-icon-1");
+    this.classList.add("bookmark-icon");
+  } else {
+    this.classList.remove("bookmark-icon");
+    this.classList.add("bookmark-icon-1");
+  }
   popup.style.display = "block";
 });
